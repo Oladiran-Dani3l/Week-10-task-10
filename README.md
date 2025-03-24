9690a1dcd351154d1c90776fda14948ca03a21eb
Diran <100717659+Oladiran-Dani3l@users.noreply.github.com>
2025-03-24 10:48:24 +0100

    Console app practicing YDKJS: this & Object prototypes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Week-10-task-10
+
+Simple console app to implement this & Object prototypes
+
+Implemented prototype chaining, class like behavior, and instances of this
--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function Animal(name) {
+    this.name = name;
+}
 
+
+Animal.prototype.speak = function() {
+    console.log(`${this.name} makes a noise.`);
+};
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this.breed = breed;
+    }
+    
+    speak() {
+        console.log(`${this.name} the ${this.breed} barks!`);
+    }
+}
+
+const genericAnimal = new Animal("Bird");
+genericAnimal.speak(); 
+
+const myDog = new Dog("Romeo", "Rottweiler");
+myDog.speak();
+
+
+console.log(myDog instanceof Dog);
+console.log(myDog instanceof Animal); 
+console.log(myDog.hasOwnProperty("name"));
+console.log(myDog.hasOwnProperty("speak"));
+
+
+const cat = Object.create(Animal.prototype);
+cat.name = "Alao the village cat";
+cat.speak(); 
+console.log(cat.name());
